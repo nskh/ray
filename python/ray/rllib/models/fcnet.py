9ed4e5eca321db4055c0510225babbb56685914c
eugenevinitsky <eugenevinitsky@airbears2-10-142-37-191.airbears2.1918.berkeley.edu>
2018-01-18 19:25:49 -0800

    integrated in multiagent code

--- python/ray/rllib/models/fcnet.py
+++ python/ray/rllib/models/fcnet.py
@@ -8,10 +8,6 @@ import tensorflow.contrib.slim as slim
 from ray.rllib.models.model import Model
 from ray.rllib.models.misc import normc_initializer
 
-USER_DATA_CONFIGS = [
-    "fcnet_tag",  # Optional tag for fcnets to allow for more than one
-]
-
 
 class FullyConnectedNetwork(Model):
     """Generic fully connected network."""
@@ -26,15 +22,6 @@ class FullyConnectedNetwork(Model):
             activation = tf.nn.relu
         print("Constructing fcnet {} {}".format(hiddens, activation))
 
-        user_data = options.get("user_data", {})
-        for k in user_data.keys():
-            if k not in USER_DATA_CONFIGS:
-                raise Exception(
-                    "Unknown config key `{}`, all keys: {}".format(k,
-                                                            USER_DATA_CONFIGS))
-        fcnet_tag = user_data.get("fcnet_tag", None)
-
-        singular = fcnet_tag is None
         with tf.name_scope("fc_net"):
             i = 1
             last_layer = inputs
--- python/ray/rllib/models/two_level_fcnet.py
+++ python/ray/rllib/models/two_level_fcnet.py
@@ -7,13 +7,6 @@ import tensorflow as tf
 from ray.rllib.models.model import Model
 from ray.rllib.models.fcnet import FullyConnectedNetwork
 
-USER_DATA_CONFIGS = [
-    "num_subpolicies",  # Number of subpolicies for a two-level network
-    "fn_choose_subpolicy",  # Function for choosing subpolicy
-    "fn_subpolicy_state",  # Function for mapping observations to subpolicy obs
-]
-
-
 class TwoLevelFCNetwork(Model):
     """
     Two-level fully connected network, consisting of a number of
@@ -22,26 +15,14 @@ class TwoLevelFCNetwork(Model):
     """
 
     def _init(self, inputs, num_outputs, options):
-        subhiddens = options.get("fcnet_hiddens", [[256, 256]] * 1)
-        fcnet_activation = options.get("fcnet_activation", "tanh")
-        if fcnet_activation == "tanh":
-            activation = tf.nn.tanh
-        elif fcnet_activation == "relu":
-            activation = tf.nn.relu
-        print("Constructing two level fcnet {} {}".format(subhiddens,
-                                                         activation))
 
-        user_data = options.get("user_data", {})
-        for k in user_data.keys():
-            if k not in USER_DATA_CONFIGS:
-                raise Exception(
-                    "Unknown config key `{}`, all keys: {}".format(k,
-                                                            USER_DATA_CONFIGS))
-        num_subpolicies = user_data.get("num_subpolicies", 1)
+        custom_options = options["custom_options"]
+        subhiddens = custom_options.get("hierarchical_fcnet_hiddens", [[256, 256]] * 1)
+        num_subpolicies = custom_options.get("num_subpolicies", 1)
         # function which maps from observation to subpolicy observation
-        to_subpolicy_state = user_data.get("fn_subpolicy_state", None)
+        to_subpolicy_state = custom_options.get("fn_subpolicy_state", None)
         # function which maps from observation to choice of subpolicy
-        fn_choose_policy = user_data.get("fn_choose_subpolicy", None)
+        fn_choose_policy = custom_options.get("fn_choose_subpolicy", None)
 
         if to_subpolicy_state is None:
             to_subpolicy_state = lambda x, k: x
@@ -62,16 +43,16 @@ class TwoLevelFCNetwork(Model):
 
         outputs = []
         for k in range(num_subpolicies):
-            sub_options = options.copy()
-            sub_options.update({"fcnet_hiddens": subhiddens[k]})
-            sub_options["user_data"] = {"fcnet_tag": k}
-            subinput = to_subpolicy_state(inputs, k)
-            fcnet = FullyConnectedNetwork(
-                subinput, num_outputs, sub_options)
-            output, last_layer = fcnet.outputs, fcnet.last_layer
-            rep_attention = tf.reshape(tf.tile(attention[:, k], [num_outputs]),
-                [-1, num_outputs])
-            outputs.append(rep_attention * output)
+            with tf.variable_scope("multi{}".format(k)):
+                sub_options = options.copy()
+                sub_options.update({"fcnet_hiddens": subhiddens[k]})
+                subinput = to_subpolicy_state(inputs, k)
+                fcnet = FullyConnectedNetwork(
+                    subinput, num_outputs, sub_options)
+                output, last_layer = fcnet.outputs, fcnet.last_layer
+                rep_attention = tf.reshape(tf.tile(attention[:, k], [num_outputs]),
+                    [-1, num_outputs])
+                outputs.append(rep_attention * output)
         overall_output = tf.add_n(outputs)
         # TODO(cathywu) check that outputs is not used later on because it's
         # a list instead of a layer
